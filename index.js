8add5a0cf4ab4fdac4f50d9fa18c310782a514cd
João Braz Simões <joaosbraz@gmail.com>
2020-01-24 03:49:41 +0000

    Add custom input field (this is hardcoded and should be fixed)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import SectionedMultiSelect from './lib/sectioned-multi-select';
 
+// FIXME: We made some changes in order to support custom forms. The style of the select text became hardcoded
+
 export default SectionedMultiSelect;
